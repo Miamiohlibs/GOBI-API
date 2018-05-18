670d49831bf05af7b140888a606a087067be00d2
Craig Boman <craig.boman@gmail.com>
2018-05-18 10:39:35 -0400

    sql queries

---
+++ gobi-test.sql
@@ -0,0 +1,26 @@
+﻿SELECT
+  b.cataloging_date_gmt, --b. v. abbreviations must be consistent throughout
+  v.marc_tag,
+  v.field_content as isbn,
+  b.is_suppressed
+  
+FROM
+sierra_view.bib_record AS b
+JOIN
+sierra_view.bib_record_location AS l
+ON
+b.record_id = l.bib_record_id
+JOIN
+sierra_view.varfield_view AS v
+ON
+b.id = v.record_id
+
+WHERE
+  b.cataloging_date_gmt >  (now() - interval '8 days') 
+AND
+  v.marc_tag = '020' --need regex exclude \|q.*
+AND
+  b.is_suppressed = 'FALSE'
+AND
+  b.bcode2 = '@'
+
